a1c1bf2aafa85efd06722c6dcd6d0428d26735aa
Binuri321 <binuriminoshi0@gmail.com>
2026-04-17 20:02:10 +0530

    Create OwnerAnomaly.js

---
+++ src/models/OwnerAnomaly.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const OwnerAnomalySchema = new mongoose.Schema(
+  {
+    room_id: { type: String, required: true, index: true },
+    date: { type: String, required: true, index: true },
+    status: { type: String, default: "Abnormal" },
+    reason: { type: String, default: "" },
+    total_energy_kwh: { type: Number, default: 0 },
+    wasted_energy_kwh: { type: Number, default: 0 },
+    anomaly_score: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+OwnerAnomalySchema.index({ room_id: 1, date: 1 }, { unique: true });
+
+module.exports = mongoose.model("OwnerAnomaly", OwnerAnomalySchema, "owner_anomalies");
